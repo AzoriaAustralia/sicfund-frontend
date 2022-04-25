cae2aea75baffe82ff019edd1a11fdd794e1ffcb
Joshua Wallace <79308061+exp0logy@users.noreply.github.com>
2022-04-25 10:28:04 +0800

    Update

--- src/components/section-components/banner.js
+++ src/components/section-components/banner.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Countdown from "react-countdown";
+
 class Banner extends Component {
 
    render() {
@@ -17,8 +17,7 @@ class Banner extends Component {
                            <div className="col-lg-9 col-sm-8">
                               <div className="banner-inner">
                                  <img src="/assets/img/HERO-IMAGE-NEW.png"  alt="SIC-FUND Island"/>
-                                 <h3>Sale ends<br></br>25th April, 3pm (AEST)</h3>
-                                 <h3><Countdown date={new Date(1650862800000)}/></h3>
+                                 <h3>Sale ends<br></br>25th April, 3pm<br></br>(AEST)</h3>
                               </div>
                            </div>
                            <div className="col-lg-3 col-sm-4">
--- src/lib/getRaisedCount.js
+++ src/lib/getRaisedCount.js
@@ -3,7 +3,7 @@ import contractAbi from "../data/contractAbi.json";
 
 export default async function getRaisedCount() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_JSON_RPC_URL);
-  const contract = new ethers.Contract(process.env.NEXT_PUBLIC_PURCHASE_CONTRACT_ADDRESS, contractAbi, provider);
+  const contract = new ethers.Contract("0x127e84af46dbf2a710e0bb8938e1f1922f831b50", contractAbi, provider);
 
   const totalBalance = await contract.totalBalance();
 
